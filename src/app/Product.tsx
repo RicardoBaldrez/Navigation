7879adcd3210c2603fe595e6ea84d08b7db9c63d
Ricardo Baldrez <rdbz@gft.com>
2025-07-11 18:33:08 -0300

    feat: Add useNavigation to handle page navigation

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,14 +1,16 @@
 import { View } from "react-native"
+import { useNavigation } from "@react-navigation/native";
 
 import Title from "@/components/Title"
 import Header from "@/components/Header"
 import ButtonIcon from "@/components/ButtonIcon";
 
 export function Product() {
+	const navigation = useNavigation();
 	return (
 		<View style={{ flex: 1, padding: 32, paddingTop: 54  }}>
 			<Header>
-				<ButtonIcon name="arrow-circle-left" />
+				<ButtonIcon name="arrow-circle-left" onPress={() => navigation.goBack()}/>
 				<Title>Product</Title>
 			</Header>
 		</View>
