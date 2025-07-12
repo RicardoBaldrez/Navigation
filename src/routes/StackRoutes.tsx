cfbb277e06f47dbfc01014d43970f3eb11170bb7
Ricardo Baldrez <rdbz@gft.com>
2025-07-12 17:38:40 -0300

    refact: Add navigation props and typying

--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -3,11 +3,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "@/app/Home";
 import { Product } from "@/app/Product";
 
-const Stack = createNativeStackNavigator();
+export type StackRoutesList = {
+	home: undefined;
+	product: undefined;
+}
+
+const Stack = createNativeStackNavigator<StackRoutesList>();
 
 export function StackRoutes() {
 	return (
-		<Stack.Navigator initialRouteName="home">
+		<Stack.Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="home" component={Home} />
 			<Stack.Screen name="product" component={Product} />
 		</Stack.Navigator>
