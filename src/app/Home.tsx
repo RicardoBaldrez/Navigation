31b1820cdcba0d26c35f0941d76c7afe3d130f98
Ricardo Baldrez <rdbz@gft.com>
2025-07-17 18:11:01 -0300

    feat: Add navigation.toggleDrawer()

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,19 +1,26 @@
-import { View } from "react-native"
+import { View } from "react-native";
 
 // import { StackRoutesProps } from "@/routes/StackRoutes";
 import { DrawerRoutesProps } from "@/routes/DrawerRoutes";
 
-import Title from "@/components/Title"
-import Header from "@/components/Header"
+import Title from "@/components/Title";
+import Header from "@/components/Header";
 import ButtonIcon from "@/components/ButtonIcon";
 
 export function Home({ navigation }: DrawerRoutesProps<"home">) {
-	return (
-		<View style={{ flex: 1, padding: 32, paddingTop: 54  }}>
-			<Header>
-				<Title>Home</Title>
-				<ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "182" })}/>
-			</Header>
-		</View>
-	);
+  return (
+    <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
+      <Header>
+        <ButtonIcon
+          name="menu"
+          onPress={() => navigation.toggleDrawer()}
+        />
+        <Title>Home</Title>
+        <ButtonIcon
+          name="add-circle"
+          onPress={() => navigation.navigate("product", { id: "182" })}
+        />
+      </Header>
+    </View>
+  );
 }
