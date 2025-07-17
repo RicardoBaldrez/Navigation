89e405fb5a55a1fe8111b195fed4096bc03f0d89
Ricardo Baldrez <rdbz@gft.com>
2025-07-17 16:33:40 -0300

    feat: Adds parameters to the route and dynamic typing

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,17 +1,23 @@
 import { View } from "react-native"
+import { useRoute } from "@react-navigation/native";
 
-import { StackRoutesProps } from "@/routes/StackRoutes";
+import { StackRoutes, StackRoutesProps } from "@/routes/StackRoutes";
 
 import Title from "@/components/Title"
 import Header from "@/components/Header"
 import ButtonIcon from "@/components/ButtonIcon";
 
+type RouteParams = StackRoutesProps<"product">;
+
 export function Product({ navigation, route }: StackRoutesProps<"product">) {
+	const { params } = useRoute<RouteParams["route"]>();
+
 	return (
 		<View style={{ flex: 1, padding: 32, paddingTop: 54  }}>
 			<Header>
 				<ButtonIcon name="arrow-circle-left" onPress={() => navigation.goBack()}/>
-				<Title>Product { route.params?.id }</Title>
+				{/* <Title>Product { route.params?.id }</Title> */}
+				<Title>Product with hook: {params?.id}</Title>
 			</Header>
 		</View>
 	);
