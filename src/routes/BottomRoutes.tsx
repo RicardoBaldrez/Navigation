96141147396cebd98063579726aec969d29c5dff
Ricardo Baldrez <rdbz@gft.com>
2025-07-17 17:50:47 -0300

    feat: Customize bottom Navigator

--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -1,22 +1,53 @@
-import { createBottomTabNavigator, BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  BottomTabScreenProps,
+} from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Home } from "@/app/Home";
 import { Product } from "@/app/Product";
 
 type BottomTabRoutesList = {
-	home: undefined;
-	product: undefined | { id: string };
-}
+  home: undefined;
+  product: undefined | { id: string };
+};
 
-export type BottomTabRoutesProps<T extends keyof BottomTabRoutesList> = BottomTabScreenProps<BottomTabRoutesList, T>;
+export type BottomTabRoutesProps<T extends keyof BottomTabRoutesList> =
+  BottomTabScreenProps<BottomTabRoutesList, T>;
 
 const BottomTab = createBottomTabNavigator<BottomTabRoutesList>();
 
 export function BottomTabRoutes() {
-	return (
-		<BottomTab.Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
-			<BottomTab.Screen name="home" component={Home} />
-			<BottomTab.Screen name="product" component={Product} />
-		</BottomTab.Navigator>
-	)
+  return (
+    <BottomTab.Navigator
+      initialRouteName="home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2c46b1",
+        tabBarInactiveTintColor: "#444444",
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
+      <BottomTab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
 }
